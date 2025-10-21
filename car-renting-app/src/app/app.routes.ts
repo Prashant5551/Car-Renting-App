680bf631edf5042b5becb93dca20fcf147ef4e62
Prashant5551 <prashkbk@gmail.com>
2025-10-21 14:35:34 +0530

    routes code added

--- car-renting-app/src/app/app.routes.ts
+++ car-renting-app/src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Layout } from './pages/layout/layout';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Vehicles } from './pages/vehicles/vehicles';
+import { Booking } from './pages/booking/booking';
+import { Customer } from './pages/customer/customer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path:'',
+    redirectTo:'login',
+    pathMatch:'full'
+    },
+    {
+        path:'login',
+        component:Login
+    },
+    {
+        path:'',
+        component:Layout,
+        children:[
+            {
+                path:'dashboard',
+                component:Dashboard
+            },
+            {
+                path:'vehicles',
+                component:Vehicles
+            },
+            {
+                path:'booking',
+                component:Booking
+            },
+            {
+                path:'customer',
+                component:Customer
+            }
+        ]
+    }
+];
