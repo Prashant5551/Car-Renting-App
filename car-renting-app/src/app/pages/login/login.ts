eeef4adfa4d8c32f424b266b77d5e17958355bfe
Prashant5551 <prashkbk@gmail.com>
2026-01-06 12:55:23 +0530

    Login code
    UI code added

--- car-renting-app/src/app/pages/login/login.ts
+++ car-renting-app/src/app/pages/login/login.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  loginObj : any = {
+    username: '',
+    password: ''
+  };
+
+  onLogin(){
+    if(this.loginObj.username === 'prashant' && this.loginObj.password ==='12345'){
+      alert('Login Successful');
+    } else {
+      alert('Invalid Credentials');
+    }
+  }
 
 }
